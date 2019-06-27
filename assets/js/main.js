95fb41f35e18f18630c9d2197758393096d447c3
Alan Borowy <borowyalan@gmail.com>
2019-06-27 10:54:09 +0200

    delete dropdown menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,34 +24,4 @@
 			}, 100);
 		});
 
-	// Nav.
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
 })(jQuery);
